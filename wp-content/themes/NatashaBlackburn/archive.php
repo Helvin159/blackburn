b0d6c5194c6ce58e33848757137353dc6db874f3
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-11 09:20:35 -0400

    update

--- wp-content/themes/NatashaBlackburn/archive.php
+++ wp-content/themes/NatashaBlackburn/archive.php
@@ -5,14 +5,17 @@
 
 <?php require_once('partials/nav.php')?>
 	<div class="container-fluid">
-		<div class="container text-center"><h1 class="display-3"><?php the_archive_title();?></h1></div>
+
+        <div class="container text-center"><h1 class="display-3"><?php $title = get_the_archive_title(); echo  substr($title, 9); ?></h1></div>
 		<div class="container">
 
 			<!--Boudoir-->
 			<div class="row py-2">
                 <?php 
                 while(have_posts()){
-                    the_post(); ?>
+                    the_post(); 
+                        
+                    ?>
                     <div class="col-md-4 my-2 text-center">
 					    <a href="<?php the_permalink()?>">
                             <img class="img-fluid imgthumbs m-auto" src="<?php the_post_thumbnail_url('pagethumbnails')?>">
