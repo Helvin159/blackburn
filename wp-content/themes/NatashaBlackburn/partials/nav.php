cb5cb218cc341a890a0fd3a3a0c38da5a0443e01
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-11 09:09:04 -0400

    update

--- wp-content/themes/NatashaBlackburn/partials/nav.php
+++ wp-content/themes/NatashaBlackburn/partials/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(0,0,0,0);">
-			<a class="navbar-brand" href="<?php echo esc_url(site_url('/'))?>">Natasha Blackburn</a>
+			<a class="navbar-brand" href="<?php echo esc_url(site_url('/home'))?>">Natasha Blackburn</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="fas fa-stream"></span>
 			</button>
