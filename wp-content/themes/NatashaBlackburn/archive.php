22c31bd487a548f0f09bddbaf592969b2028fe7f
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-11 09:05:12 -0400

    update

--- wp-content/themes/NatashaBlackburn/archive.php
+++ wp-content/themes/NatashaBlackburn/archive.php
@@ -13,9 +13,9 @@
                 <?php 
                 while(have_posts()){
                     the_post(); ?>
-                    <div class="col-md-3 my-2">
+                    <div class="col-md-3 my-2 text-center">
 					    <a href="<?php the_permalink()?>">
-                            <img class="img-fluid imgthumbs" src="<?php the_post_thumbnail_url('pagethumbnails')?>">
+                            <img class="img-fluid imgthumbs m-auto" src="<?php the_post_thumbnail_url('pagethumbnails')?>">
                         </a>
 				    </div>
                 <?php }
