fbbedf415901a0e274cd9de272e1fe4a671db9b8
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-12 12:42:15 -0400

    update config

--- wp-config.php
+++ wp-config.php
@@ -25,6 +25,11 @@ if(strstr($_SERVER['SERVER_NAME'], 'modelportfolio.local')){
 	define( 'DB_USER', 'root' );
 	define( 'DB_PASSWORD', 'root' );
 	define( 'DB_HOST', 'localhost' );
+}else if(strstr($_SERVER['SERVER_NAME'], 'mrrymer.com')){
+    define( 'DB_NAME', 'kmmkcamy_nblackburn' );
+	define( 'DB_USER', 'kmmkcamy_helvin' );
+	define( 'DB_PASSWORD', 'Dora0411!?!' );
+	define( 'DB_HOST', 'localhost' );
 }else{
 	define( 'DB_NAME', 'nblackburn' );
 	define( 'DB_USER', 'helvin' );
